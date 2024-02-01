64ab07194367967b01c769fddc89f7b264a16836
vivek bansal <vivekbansal19@gmail.com>
2024-02-01 11:12:29 -0500

    feat: Added exception handling with user  friendly messages on HomeViewModel.

--- app/src/androidTest/java/com/vivek/githubapisample/home/presentation/HomeViewModelTest.kt
+++ app/src/androidTest/java/com/vivek/githubapisample/home/presentation/HomeViewModelTest.kt
@@ -16,15 +16,18 @@ import com.vivek.githubapisample.user.data.User
 import com.vivek.githubapisample.user.domain.GetUserInfoUsecase
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
+import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.test.StandardTestDispatcher
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
+import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -41,7 +44,7 @@ class HomeViewModelTest {
     @MockK(relaxed = true)
     lateinit var repository: RemoteUserRepository
 
-    lateinit var getUserInfoUsecase: GetUserInfoUsecase
+    private lateinit var getUserInfoUsecase: GetUserInfoUsecase
 
     @MockK
     lateinit var getReposByUsernameUsecase: GetReposByUsernameUsecase
@@ -58,16 +61,19 @@ class HomeViewModelTest {
     @Before
     fun setUp() {
         MockKAnnotations.init(this)
-
         getUserInfoUsecase = GetUserInfoUsecase(repository)
+    }
+
+    private fun defaultSetup() {
+        savedStateHandle = SavedStateHandle().apply {
+            set("user_name", null)
+        }
 
         coEvery {
             networkMonitor.isOnline
         } returns flowOf(true)
 
-        savedStateHandle = SavedStateHandle().apply {
-            set("user_name", null)
-        }
+
         sut = HomeViewModel(
             getUserInfoUsecase,
             getReposByUsernameUsecase,
@@ -78,6 +84,8 @@ class HomeViewModelTest {
 
     @Test
     fun stateIsInitiallyDefault() = runTest {
+        defaultSetup()
+
         assertEquals(
             HomeUiState(),
             sut.state.value,
@@ -86,16 +94,22 @@ class HomeViewModelTest {
 
     @Test
     fun doSearchShouldUpdateSavedStateHandle() = coroutineRule.runBlockingTest {
+        // Arrange
+        defaultSetup()
 
+        // Act
         val uiAction = HomeUiAction.DoSearch(query)
         sut.handleUiAction(uiAction)
         delay(10)
+
+        // Assert
         assertEquals(query, savedStateHandle[HomeViewModel.USER_NAME_KEY])
     }
 
     @Test
     fun doSearchShouldNotFetchUserOnInvalidQuery() = runTest {
         // Arrange
+        defaultSetup()
         coEvery { repository.fetchUserInfo(any()) } returns Result.success(User.fake())
 
         // Act
@@ -112,6 +126,7 @@ class HomeViewModelTest {
     @Test
     fun doSearchShouldFetchUserOnValidQuery() = runTest {
         // Arrange
+        defaultSetup()
         coEvery {
             repository.fetchUserInfo(query)
         } returns Result.success(User.fake())
@@ -132,6 +147,7 @@ class HomeViewModelTest {
     @Test
     fun doSearchShouldReturnFailureIfNoUserFound() = runTest {
         // Arrange
+        defaultSetup()
         coEvery { repository.fetchUserInfo(query) } returns Result.failure(AppException.NotFound())
 
         // Act
@@ -148,10 +164,16 @@ class HomeViewModelTest {
 
     @Test
     fun updateUsernameSearchShouldUpdateUsernameSearchFlow() = runTest {
+        // Arrange
+        defaultSetup()
         val collectJob1 =
             launch(StandardTestDispatcher()) { sut.state.collect() }
+
+        // Act
         sut.handleUiAction(HomeUiAction.UpdateUsernameSearch(query))
         delay(10)
+
+        // Assert
         assertEquals(query, sut.state.value.usernameQuery)
 
         collectJob1.cancel()
@@ -160,6 +182,8 @@ class HomeViewModelTest {
     @Test
     fun reposByUserFlowShouldEmitPagingDataWhenUserIsValid() = runTest {
 
+        // Arrange
+        defaultSetup()
         val pagingData = PagingData.from(listOf(Repo.fake()))
 
         coEvery {
@@ -179,4 +203,39 @@ class HomeViewModelTest {
             assertNotNull(awaitItem())
         }
     }
+
+    @Test
+    fun isOnlineFlowShouldReturnFalseOnException() = runTest {
+
+        // Arrange
+        savedStateHandle = SavedStateHandle().apply {
+            set("user_name", null)
+        }
+
+        every {
+            networkMonitor.isOnline
+        } returns flow {
+            throw IllegalArgumentException("Simulated error")
+        }
+
+        sut = HomeViewModel(
+            getUserInfoUsecase,
+            getReposByUsernameUsecase,
+            savedStateHandle,
+            networkMonitor
+        )
+
+
+        sut.state.test {
+            // Assert
+            awaitItem()
+            awaitItem()
+
+            val result = awaitItem()
+
+            assertTrue(!result.isOnline)
+            assertNotNull(result.message)
+        }
+    }
+
 }
